5a930fe1af235cc2e953a4af76900e309c130d30
Nityams <nityamshrestha@gmail.com>
2017-12-04 17:45:09 -0800

    facebook with user info

---
+++ app/src/main/java/nityam/com/career/Model/PrefUtil.java
@@ -0,0 +1,64 @@
+package nityam.com.career.Model;
+
+/**
+ * Created by nityamshrestha on 12/4/17.
+ */
+
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+/**
+ * Created by Kuray(FreakyCoder) on 25/08/16.
+ */
+
+public class PrefUtil {
+
+    private Activity activity;
+
+    // Constructor
+    public PrefUtil(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void saveAccessToken(String token) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("fb_access_token", token);
+        editor.apply(); // This line is IMPORTANT !!!
+    }
+
+
+    public String getToken() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        return prefs.getString("fb_access_token", null);
+    }
+
+    public void clearToken() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.clear();
+        editor.apply(); // This line is IMPORTANT !!!
+    }
+
+    public void saveFacebookUserInfo(String first_name,String last_name, String email, String gender, String profileURL){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("fb_first_name", first_name);
+        editor.putString("fb_last_name", last_name);
+        editor.putString("fb_email", email);
+        editor.putString("fb_gender", gender);
+        editor.putString("fb_profileURL", profileURL);
+        editor.apply(); // This line is IMPORTANT !!!
+        Log.d("MyApp", "Shared Name : "+first_name+"\nLast Name : "+last_name+"\nEmail : "+email+"\nGender : "+gender+"\nProfile Pic : "+profileURL);
+    }
+
+    public void getFacebookUserInfo(){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        Log.d("MyApp", "Name : "+prefs.getString("fb_name",null)+"\nEmail : "+prefs.getString("fb_email",null));
+    }
+
+
+}
---
+++ app/src/main/java/nityam/com/career/View/Login.java
@@ -0,0 +1,203 @@
+package nityam.com.career.View;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.share.widget.ShareDialog;
+
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+
+import nityam.com.career.Model.PrefUtil;
+import nityam.com.career.R;
+
+
+public class Login extends AppCompatActivity {
+
+    com.facebook.login.widget.LoginButton loginButton;
+    private CallbackManager callbackManager;
+    ShareDialog shareDialog;
+    private PrefUtil prefUtil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        prefUtil = new PrefUtil(this);
+
+
+        getSupportActionBar().hide();
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        setContentView(R.layout.activity_login);
+
+    //---------------------------------FACEBOOK----------------------------------------------------------
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // put them on log
+//        Log.d("<onActResult-ReqCode> ",Integer.toString(requestCode));
+//        Log.d("<onActResult-ResCode> ",Integer.toString(resultCode));
+//        Log.d("<onActResult-IntData> ",data.toString());
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    public void fbOnClick(View view){
+
+        loginButton.setReadPermissions("email");
+
+        // Callback registration
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile","email"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.d("<regCallback-Succ> ",loginResult.toString());
+                String accessToken = loginResult.getAccessToken().getToken();
+
+                // save accessToken to SharedPreference
+                Log.d("<TOKEN>",accessToken.toString());
+                prefUtil.saveAccessToken(accessToken);
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject jsonObject,
+                                                    GraphResponse response) {
+
+                                // Getting FB User Data
+                                Bundle facebookData = getFacebookData(jsonObject);
+
+                                Log.d("<FB-DATA>",facebookData.toString());
+
+                            }
+                        });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,first_name,last_name,email,gender");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+
+
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("<regCallback-Succ> "," Cancelled");
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Log.d("<regCallback-Succ> ","error");
+            }
+        });
+
+
+
+    }
+
+    private Bundle getFacebookData(JSONObject object) {
+        Bundle bundle = new Bundle();
+
+        try {
+            String id = object.getString("id");
+            URL profile_pic;
+            try {
+                profile_pic = new URL("https://graph.facebook.com/" + id + "/picture?type=large");
+                Log.i("profile_pic", profile_pic + "");
+                bundle.putString("profile_pic", profile_pic.toString());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            bundle.putString("idFacebook", id);
+            if (object.has("first_name"))
+                bundle.putString("first_name", object.getString("first_name"));
+            if (object.has("last_name"))
+                bundle.putString("last_name", object.getString("last_name"));
+            if (object.has("email"))
+                bundle.putString("email", object.getString("email"));
+            if (object.has("gender"))
+                bundle.putString("gender", object.getString("gender"));
+
+
+            prefUtil.saveFacebookUserInfo(object.getString("first_name"),
+                    object.getString("last_name"),object.getString("email"),
+                    object.getString("gender"), profile_pic.toString());
+
+        } catch (Exception e) {
+            Log.d("<NITYAM-BundleError>", "BUNDLE Exception : "+e.toString());
+        }
+
+        return bundle;
+    }
+
+
+    public void signupOnClick(View view) {
+        Toast.makeText(this, " SIgn Up ", Toast.LENGTH_SHORT).show();
+    }
+
+    public void signinOnClick(View view){
+        // disabled
+        Toast.makeText(this, " Sign In ", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+    /**
+     * Might need to use this during log out
+     *
+    private void deleteAccessToken() {
+        AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
+
+                if (currentAccessToken == null){
+                    //User logged out
+                    prefUtil.clearToken();
+                    LoginManager.getInstance().logOut();
+                }
+            }
+        };
+    }
+    */
+}
