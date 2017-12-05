20e6620d3866403a818f78c095bc0fa3e9f72545
Nityams <nityamshrestha@gmail.com>
2017-12-05 13:36:24 -0800

    new application UI, with wrong colors

---
+++ app/src/main/java/com/nityam/career/JobPage.java
@@ -0,0 +1,15 @@
+package com.nityam.career;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JobPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_page);
+
+
+    }
+}
